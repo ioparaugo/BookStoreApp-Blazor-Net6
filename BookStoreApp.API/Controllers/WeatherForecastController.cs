18304bc4486126b8118345d961368c0781c6da09
Ikenna <ike007007@gmail.com>
2022-09-25 23:42:36 +0400

    Add project files.

---
+++ BookStoreApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Calling the Weather API");
+            throw new Exception("This is a new Exception");
+            try
+            {
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+           .ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message,"Fatal Error Occurred");
+                throw;
+            }
+           
+        }
+    }
+}
